484daafb7dd64b3be52a69f6f66060b42d061731
mikolajbendyk <mikolaj.bendyk@gmail.com>
2021-04-13 15:42:36 +0200

    CountDiv

---
+++ src/main/java/codility/lessons/CountDiv.java
@@ -0,0 +1,16 @@
+package codility.lessons;
+
+public class CountDiv {
+
+    public static void main(String[] args) {
+        System.out.println(solution(6,11,2));
+    }
+
+    public static int solution(int A, int B, int K) {
+        int counter = 0;
+        for (int i=A; i<=B; i++) {
+            counter = i % K == 0 ? counter+1 : counter;
+        }
+        return counter;
+    }
+}
