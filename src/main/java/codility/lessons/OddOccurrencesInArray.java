31db3d247a25f99ce10453a159eed9769538de52
mikolajbendyk <mikolaj.bendyk@gmail.com>
2022-10-22 15:22:48 +0200

    odd occurrences

--- src/main/java/codility/lessons/OddOccurrencesInArray.java
+++ src/main/java/codility/lessons/OddOccurrencesInArray.java
@@ -1,6 +1,10 @@
 package codility.lessons;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class OddOccurrencesInArray {
 
@@ -23,7 +27,7 @@ public class OddOccurrencesInArray {
                 if (i % 2 != 0) {
                     return A[i-1];
                 }
-                if (i == A.length-1) {
+                if (i == A.length-1) {  //to w sumie mogłoby być nizej zamiast return 0, bo w tresci zad info ze tablica    zawsze bedzie miala jeden element bez powtorzen
                     return A[i];
                 }
                 comp = A[i];
@@ -31,10 +35,8 @@ public class OddOccurrencesInArray {
         }
         return 0;
     }
-}
 
-
-//    List<Integer> list = Arrays.stream(A)
+    //    List<Integer> list = Arrays.stream(A)
 //                .filter(n -> {
 //                    int counter = 0;
 //                    for (int i : A) {
@@ -45,3 +47,55 @@ public class OddOccurrencesInArray {
 //                    return counter % 2 == 0;
 //                })
 //                .collect(Collectors.toList());
+
+
+
+
+    private static int solution2(int[] A) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < A.length; i++) {
+            Integer val = map.get(A[i]);
+            if (val == null || val == 2) {
+                map.put(A[i], 1);
+            } else {
+                map.put(A[i], 2);
+            }
+        }
+
+        return map.keySet().stream().filter(k -> map.get(k) == 1).findFirst().orElse(0);
+    }
+
+
+    // for (int i = 0; i < A.length; i++) {
+//     Integer val = map.get(A[i]);
+//     if (val == null) {
+//         map.put(A[i], 1);
+//     } else {
+//         if (val == 1) {
+//             map.put(A[i], 2);
+//         } else {
+//             map.remove(A[i]);
+//         }
+//     }
+// }
+
+
+    private static int solutionJavaStart(int[] A) {
+        Set<Integer> ints = new HashSet<>();
+        
+        for (int i : A) {
+            if (ints.contains(i)) {
+                ints.remove(i);
+            } else {
+                ints.add(i);
+            }
+        }
+
+        for (int i : ints) {
+            return i;
+        }
+
+        return 0;
+    }
+}
