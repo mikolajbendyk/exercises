aa07fa08d790015f2bebad602aeeba6e5521ced3
mikolajbendyk <mikolaj.bendyk@gmail.com>
2022-10-22 16:27:15 +0200

    frog jump

--- src/main/java/codility/lessons/FrogJmp.java
+++ src/main/java/codility/lessons/FrogJmp.java
@@ -14,4 +14,15 @@ public class FrogJmp {
             return (div / D) + 1;
         }
     }
+
+    private static int solution2(int X, int Y, int D) {
+        if (X == Y) return 0;
+        int road = Y - X;
+        int jumps = road / D;
+        if (jumps * D >= road) {
+            return jumps;
+        } else {
+            return jumps + 1;
+        }
+    }
 }
