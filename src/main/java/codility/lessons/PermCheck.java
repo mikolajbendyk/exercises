18fdbb47ee5497126592deab6cee3ede51388342
mikolajbendyk <mikolaj.bendyk@gmail.com>
2022-10-24 21:27:22 +0200

    perm check

--- src/main/java/codility/lessons/PermCheck.java
+++ src/main/java/codility/lessons/PermCheck.java
@@ -15,7 +15,7 @@ public class PermCheck {
         int permNum = 1;
 
         for (int i : A) {
-            if (permNum == i) {
+            if (permNum == i) {     // WTF? xD
                 permNum++;
             } else {
                 return 0;
@@ -24,5 +24,28 @@ public class PermCheck {
 
         return 1;
 
+    }
+
+    private static int solution2(int[] A) {
+        Arrays.sort(A);
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != i + 1) return 0;
+        }
+        return 1;
+    }
+
+    private static int solution3(int[] A) {
+        boolean[] check = new boolean[A.length];
+        try {
+            for (int i : A) {
+                check[i - 1] = true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return 0;
+        }
+        for (boolean b : check) {
+            if (!b) return 0;
+        }
+        return 1;
     }
 }
